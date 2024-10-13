c9d1bb536cd66afc35b8903bdd80bb3d6f7fb77a
sarah cristina <sarahsilva34567@gmail.com>
2024-10-13 19:03:39 -0300

    Cria o arquivo peças.ts, o construtor e função desenhar para peças

---
+++ vite-project/src/pecas.ts
@@ -0,0 +1,24 @@
+export default class Pecas {
+
+    forma: string;
+    cor: string;
+
+    constructor(forma: string, cor: string){
+        this.forma = forma;
+        this.cor = cor;
+
+    }
+};
+
+desenhar(): string{
+
+   const template = `
+    <div class= "peca"
+    style = "background-color: ${this.cor};
+    text-align: center;
+    line-height: 50px;>
+    ${this.forma};
+    </div>
+`;
+return template;
+}
